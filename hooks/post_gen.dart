ec713da14d1b6d3041f8464d9cb19e9a95b45958
Gabriele Bilello <56848837+Gabriele-bil@users.noreply.github.com>
2024-04-12 14:56:37 +0200

    feat: add git commit in post gen

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -3,6 +3,8 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
   await runPnpm(context: context);
+
+  await runGit(context: context);
 }
 
 Future<void> runPnpm({
@@ -11,4 +13,19 @@ Future<void> runPnpm({
   context.logger.info('📦 Running pnpm i');
   await Process.run('pnpm', ['i']);
   context.logger.success('📦 pnpm configured successfully 🚀');
+}
+
+Future<void> runGit({
+  required HookContext context,
+}) async {
+  context.logger.info('📚 Staging initial scaffold files...');
+  await Process.run('git', ['add', '.'],);
+
+  context.logger.info('📚 Committing "chore(scaffold): angular application"...');
+  await Process.run(
+    'git',
+    ['commit', '-m', '"chore(scaffold): angular application"'],
+  );
+
+  context.logger.success('📚 Git commited successfully! 🚀');
 }
