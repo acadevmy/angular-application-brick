aaae0b96af643be1f06e1f1eb3b1a9c47afb23df
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-05-21 16:13:47 +0200

    feat: added dotenv to test

--- __brick__/jest.config.js
+++ __brick__/jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
+  // Required for ESM support
+  preset: "jest-preset-angular/presets/defaults-esm",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  testMatch: ["<rootDir>/src/app/**/*.(spec|jest).ts"],
+  moduleNameMapper: {
+    "^rxjs(/operators$)?$":
+      "<rootDir>/node_modules/rxjs/dist/bundles/rxjs.umd.js",
+    tslib: "<rootDir>/node_modules/tslib/tslib.es6.mjs",
+  },
+
+  // @dotenv-run/jest-angular transformer
+  transform: {
+    "^.+\\.(ts)$": [
+      "@dotenv-run/jest-angular",
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
--- __brick__/src/setup.jest.ts
+++ __brick__/src/setup.jest.ts
@@ -1,1 +1,7 @@
-import 'jest-preset-angular/setup-jest';
+import 'jest-preset-angular/setup-jest.mjs';
+import { env } from '@dotenv-run/core';
+
+env({
+  root: '../../..',
+  prefix: "{{applicationName.constantCase()}}_",
+});
