e6f97f1fde99c33b143a6d022a7e9fbd06005d3c
Gabriele Bilello <56848837+Gabriele-bil@users.noreply.github.com>
2024-05-30 10:42:09 +0200

    feat: moved postgen scripts to devmy cli

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,42 +1,4 @@
 import 'dart:io';
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  await runPnpm(context: context);
-
-  runGit(context: context);
-}
-
-Future<void> runPnpm({
-  required HookContext context,
-}) async {
-  context.logger.info('📦 Running pnpm i');
-  await Process.run('pnpm', ['i']);
-  context.logger.success('📦 pnpm configured successfully 🚀');
-}
-
-Future<void> runGit({
-  required HookContext context,
-}) async {
-  final applicationName = context.vars['applicationName'].toString();
-
-  context.logger.info('📚 Staging initial angular application files...');
-  await Process.run(
-    'git',
-    ['add', '.'],
-  );
-
-  context.logger.info(
-    '📚 Committing "chore(${applicationName.paramCase}): created angular application"...',
-  );
-  await Process.run(
-    'git',
-    [
-      'commit',
-      '-m',
-      '"chore(${applicationName.paramCase}): created angular application"'
-    ],
-  );
-
-  context.logger.success('📚 Git commited successfully! 🚀');
-}
+void run(HookContext context) async {}
