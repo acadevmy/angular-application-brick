e5f54e00c2d8d607844e98dd48aa496e1bcc6481
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-07-16 10:46:18 +0200

    fix: post gen message

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,23 +1,22 @@
 import 'package:mason/mason.dart';
+import 'package:chalkdart/chalk.dart';
 
 void run(HookContext context) async {
   logSentryConfiguration(context: context);
 }
 
 void logSentryConfiguration({required HookContext context}) {
-  context.logger.info(
-    '🛠️ To configure sentry, add values to the following environment variables:',
-  );
+  print(chalk.blue(
+    '🛠️ To configure Sentry, add values to the following environment variables:',
+  ));
 
   String applicationName = context.vars['applicationName'];
 
-  context.logger.write(' ${applicationName}_SENTRY_DNS');
-  context.logger.write(' ${applicationName}_SENTRY_TRACE_SAMPLE_RATE');
-  context.logger.write(' ${applicationName}_SENTRY_REPLAY_SAMPLE_RATE');
-  context.logger.write(
-    ' ${applicationName}_SENTRY_REPLAY_ON_ERROR_SAMPLE_RATE',
-  );
-  context.logger.write(' ${applicationName}_SENTRY_TRACE_PROPAGATION_TARGETS');
-  context.logger.write(' ${applicationName}_BASE_API_URL');
-  context.logger.write(' ${applicationName}_FORCE_UPDATE_INFORMATION_PARAM');
+  print('   ${applicationName}_SENTRY_DNS');
+  print('   ${applicationName}_SENTRY_TRACE_SAMPLE_RATE');
+  print('   ${applicationName}_SENTRY_REPLAY_SAMPLE_RATE');
+  print('   ${applicationName}_SENTRY_REPLAY_ON_ERROR_SAMPLE_RATE');
+  print('   ${applicationName}_SENTRY_TRACE_PROPAGATION_TARGETS');
+  print('   ${applicationName}_BASE_API_URL');
+  print('   ${applicationName}_FORCE_UPDATE_INFORMATION_PARAM\n');
 }
